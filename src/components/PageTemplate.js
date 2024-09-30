f28554a8b6b3ea92b33d2f3f7c240195659af857
Elisa <elisa.lupinj@gmail.com>
2024-09-30 15:34:37 -0700

    added header to pages

--- src/components/PageTemplate.js
+++ src/components/PageTemplate.js
@@ -1,16 +1,32 @@
 // src/components/PageTemplate.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../assets/style/PageTemplate.css'; // Import the CSS file
 
+import galaxyImageUrl from '../assets/images/galaxy.png';
+
 const PageTemplate = ({ title, children }) => {
+  const navigate = useNavigate(); // Hook to programmatically navigate
+
+  const goHome = () => {
+    navigate('/'); // Navigate to the home page
+  };
+
   return (
     <div className="page-template">
       <header>
-        <h1>{title}</h1>
+        <button className="home" onClick={goHome}>
+          &lt;
+          <img src={galaxyImageUrl} width="40px"/>
+          Go back to space
+        </button>
         {/* Add other common header elements here */}
       </header>
       <main>
-        {children} {/* This is where the specific page content will be rendered */}
+        <div className="content">
+          <h1>{title}</h1>
+          {children} {/* This is where the specific page content will be rendered */}
+        </div>
       </main>
       <footer>
         {/* Add footer elements here if needed */}
